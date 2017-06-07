5c6089a81777ab5230dc43790853f5fbf85c3ba0
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2017-06-07 22:28:59 +0800

    Add Workload changing ParamGen

---
+++ src/main/java/org/elasql/bench/rte/micro/ElasqlMicroCoAccessbenchmarkParamGen.java
@@ -0,0 +1,351 @@
+package org.elasql.bench.rte.micro;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.elasql.bench.micro.ElasqlMicrobenchConstants;
+import org.elasql.bench.util.ElasqlBenchProperties;
+import org.elasql.storage.metadata.PartitionMetaMgr;
+import org.vanilladb.bench.TransactionType;
+import org.vanilladb.bench.micro.MicroTransactionType;
+import org.vanilladb.bench.rte.TxParamGenerator;
+import org.vanilladb.bench.tpcc.TpccValueGenerator;
+import org.vanilladb.bench.util.RandomNonRepeatGenerator;
+
+public class ElasqlMicroCoAccessbenchmarkParamGen implements TxParamGenerator {
+
+	// Transaaction Type
+	private static final double DIST_TX_RATE;
+	private static final double RW_TX_RATE;
+	private static final double SKEW_TX_RATE;
+	private static final double LONG_READ_TX_RATE;
+
+	// Read Counts
+	private static final int TOTAL_READ_COUNT;
+	private static final int LOCAL_HOT_COUNT;
+	private static final int REMOTE_HOT_COUNT;
+	private static final int REMOTE_COLD_COUNT;
+	// The local cold count will be calculated on the fly
+
+	// Access Pattern
+	private static final double WRITE_RATIO_IN_RW_TX;
+	private static final double HOT_CONFLICT_RATE;
+	private static final double SKEW_PERCENTAGE;
+
+	// Data Size
+	private static final int DATA_SIZE_PER_PART;
+	private static final int HOT_DATA_SIZE_PER_PART;
+	private static final int COLD_DATA_SIZE_PER_PART;
+
+	// Shortcuts
+	static final int NUM_PARTITIONS = PartitionMetaMgr.NUM_PARTITIONS;
+
+	// Other parameters
+	private static final int RANDOM_SWAP_FACTOR = 20;
+
+	private static final int COACCESS_RANGE;
+	private static final int COACCESS_COUNT;
+	private static final double COACCESS_RATIO;
+	private static final double SKEW_RATIO;
+
+	static {
+		DIST_TX_RATE = ElasqlBenchProperties.getLoader()
+				.getPropertyAsDouble(ElasqlMicrobenchParamGen.class.getName() + ".DIST_TX_RATE", 0.0);
+		RW_TX_RATE = ElasqlBenchProperties.getLoader()
+				.getPropertyAsDouble(ElasqlMicrobenchParamGen.class.getName() + ".RW_TX_RATE", 0.0);
+		SKEW_TX_RATE = ElasqlBenchProperties.getLoader()
+				.getPropertyAsDouble(ElasqlMicrobenchParamGen.class.getName() + ".SKEW_TX_RATE", 0.0);
+		LONG_READ_TX_RATE = ElasqlBenchProperties.getLoader()
+				.getPropertyAsDouble(ElasqlMicrobenchParamGen.class.getName() + ".LONG_READ_TX_RATE", 0.0);
+		
+		TOTAL_READ_COUNT = ElasqlBenchProperties.getLoader()
+				.getPropertyAsInteger(ElasqlMicrobenchParamGen.class.getName() + ".TOTAL_READ_COUNT", 10);
+		LOCAL_HOT_COUNT = ElasqlBenchProperties.getLoader()
+				.getPropertyAsInteger(ElasqlMicrobenchParamGen.class.getName() + ".LOCAL_HOT_COUNT", 1);
+		REMOTE_HOT_COUNT = ElasqlBenchProperties.getLoader()
+				.getPropertyAsInteger(ElasqlMicrobenchParamGen.class.getName() + ".REMOTE_HOT_COUNT", 0);
+		REMOTE_COLD_COUNT = ElasqlBenchProperties.getLoader()
+				.getPropertyAsInteger(ElasqlMicrobenchParamGen.class.getName() + ".REMOTE_COLD_COUNT", 5);
+		
+		WRITE_RATIO_IN_RW_TX = ElasqlBenchProperties.getLoader()
+				.getPropertyAsDouble(ElasqlMicrobenchParamGen.class.getName() + ".WRITE_RATIO_IN_RW_TX", 0.5);
+		HOT_CONFLICT_RATE = ElasqlBenchProperties.getLoader()
+				.getPropertyAsDouble(ElasqlMicrobenchParamGen.class.getName() + ".HOT_CONFLICT_RATE", 0.01);
+		SKEW_PERCENTAGE = ElasqlBenchProperties.getLoader()
+				.getPropertyAsDouble(ElasqlMicrobenchParamGen.class.getName() + ".SKEW_PERCENTAGE", 0.2);
+
+		DATA_SIZE_PER_PART = ElasqlMicrobenchConstants.NUM_ITEMS_PER_NODE;
+		HOT_DATA_SIZE_PER_PART = (int) (1.0 / HOT_CONFLICT_RATE);
+
+		COLD_DATA_SIZE_PER_PART = DATA_SIZE_PER_PART - HOT_DATA_SIZE_PER_PART;
+
+		COACCESS_RANGE = 30000;
+		COACCESS_COUNT = 2;
+		COACCESS_RATIO = 0.4;
+		SKEW_RATIO = 0.6;
+
+	}
+
+	private Random random = new Random();
+
+	public ElasqlMicroCoAccessbenchmarkParamGen() {
+
+	}
+
+	@Override
+	public TransactionType getTxnType() {
+		return MicroTransactionType.MICRO;
+	}
+
+	// a main application for debugging
+	public static void main(String[] args) {
+		ElasqlMicroCoAccessbenchmarkParamGen executor = new ElasqlMicroCoAccessbenchmarkParamGen();
+
+		System.out.println("Parameters:");
+		System.out.println("Distributed Tx Rate: " + DIST_TX_RATE);
+		System.out.println("Read Write Tx Rate: " + RW_TX_RATE);
+		System.out.println("Skew Tx Rate: " + SKEW_TX_RATE);
+		System.out.println("Long Read Tx Rate: " + LONG_READ_TX_RATE);
+		System.out.println("Total Read Count: " + TOTAL_READ_COUNT);
+		System.out.println("Local Hot Count: " + LOCAL_HOT_COUNT);
+		System.out.println("Remote Hot Count: " + REMOTE_HOT_COUNT);
+		System.out.println("Remote Cold Count: " + REMOTE_COLD_COUNT);
+		System.out.println("Write Ratio in RW Tx: " + WRITE_RATIO_IN_RW_TX);
+		System.out.println("Hot Conflict Rate: " + HOT_CONFLICT_RATE);
+		System.out.println("Skew Percentage: " + SKEW_PERCENTAGE);
+
+		System.out.println("# of items / partition: " + DATA_SIZE_PER_PART);
+		System.out.println("# of hot items / partition: " + HOT_DATA_SIZE_PER_PART);
+		System.out.println("# of cold items / partition: " + COLD_DATA_SIZE_PER_PART);
+
+		System.out.println();
+
+		for (int i = 0; i < 100; i++) {
+			Object[] params = executor.generateParameter();
+			System.out.print(params.length + " ");
+			System.out.println(Arrays.toString(params));
+
+		}
+	}
+
+	@Override
+	public Object[] generateParameter() {
+		TpccValueGenerator rvg = new TpccValueGenerator();
+		ArrayList<Object> paramList = new ArrayList<Object>();
+
+		// ================================
+		// Decide the types of transactions
+		// ================================
+
+		boolean isDistributedTx = (rvg.randomChooseFromDistribution(DIST_TX_RATE, 1 - DIST_TX_RATE) == 0) ? true
+				: false;
+		boolean isReadWriteTx = (rvg.randomChooseFromDistribution(RW_TX_RATE, 1 - RW_TX_RATE) == 0) ? true : false;
+		boolean isSkewTx = (rvg.randomChooseFromDistribution(SKEW_TX_RATE, 1 - SKEW_TX_RATE) == 0) ? true : false;
+		boolean isLongReadTx = (rvg.randomChooseFromDistribution(LONG_READ_TX_RATE, 1 - LONG_READ_TX_RATE) == 0) ? true
+				: false;
+
+		boolean isSkew = false;
+
+		if (NUM_PARTITIONS < 2)
+			isDistributedTx = false;
+
+		// =========================================
+		// Decide the counts and the main partitions
+		// =========================================
+
+		// Choose the main partition
+		int mainPartition = 0;
+
+		//
+		int skewParirion = 0;
+
+		if (rvg.nextDouble() > SKEW_RATIO)
+			mainPartition = rvg.number(0, NUM_PARTITIONS - 1);
+		else {
+			mainPartition = skewParirion;
+			isSkew = true;
+		}
+
+		// Set read counts
+		int totalReadCount = TOTAL_READ_COUNT;
+		int localHotCount = LOCAL_HOT_COUNT;
+		int remoteHotCount = 0;
+		int remoteColdCount = 0;
+
+		// For distributed tx
+		if (isDistributedTx) {
+			remoteHotCount = REMOTE_HOT_COUNT;
+			remoteColdCount = REMOTE_COLD_COUNT;
+		}
+
+		// For long read tx
+		// 10 times of total records and remote colds
+		if (isLongReadTx) {
+			totalReadCount *= 10;
+			remoteColdCount *= 10;
+		}
+
+		// Local cold
+
+		int localColdCount;
+		if (!isSkew)
+			localColdCount = totalReadCount - localHotCount - remoteHotCount - remoteColdCount;
+		else
+			localColdCount = totalReadCount - localHotCount - remoteHotCount - COACCESS_COUNT;
+
+		// Write Count
+		int writeCount = 0;
+
+		if (isReadWriteTx) {
+			writeCount = (int) (totalReadCount * WRITE_RATIO_IN_RW_TX);
+		}
+
+		// =====================
+		// Generating Parameters
+		// =====================
+
+		// Set read count
+		paramList.add(totalReadCount);
+
+		// Choose local hot records
+		chooseHotData(paramList, mainPartition, localHotCount);
+
+		// Choose local cold records
+		chooseColdData(paramList, mainPartition, localColdCount);
+
+		// Choose remote records
+		if (!isSkew) {
+			if (isDistributedTx) {
+
+				// randomly choose hot data from other partitions
+				int[] partitionHotCount = new int[NUM_PARTITIONS];
+				// This line could be unnecessary
+				partitionHotCount[mainPartition] = 0;
+
+				// randomly choose cold data from other partitions
+				int[] partitionColdCount = new int[NUM_PARTITIONS];
+				partitionColdCount[mainPartition] = 0;
+
+				// Random gen assign remote partition source
+				for (int i = 0; i < remoteHotCount; i++) {
+					int remotePartition = randomChooseOtherPartition(mainPartition, rvg);
+					partitionHotCount[remotePartition]++;
+				}
+
+				for (int i = 0; i < NUM_PARTITIONS; i++)
+					chooseHotData(paramList, i, partitionHotCount[i]);
+
+				for (int i = 0; i < remoteColdCount; i++) {
+					int remotePartition = randomChooseOtherPartition(mainPartition, rvg);
+					partitionColdCount[remotePartition]++;
+				}
+
+				for (int i = 0; i < NUM_PARTITIONS; i++)
+					chooseColdData(paramList, i, partitionColdCount[i]);
+
+			}
+
+		} else {
+			int CoaccessCount = COACCESS_COUNT;
+			if (rvg.nextDouble() < SKEW_RATIO) {
+				System.out.println("Co-access");
+				int remotePartition = 1;
+				chooseCoaccessData(paramList, remotePartition, CoaccessCount);
+			} else {
+				System.out.println("Not Co-access");
+				chooseColdData(paramList, mainPartition, CoaccessCount);
+			}
+		}
+
+		// Set write count
+		paramList.add(writeCount);
+
+		// Choose write records
+		if (writeCount > 0) {
+			// A read-write tx must at least update one hot record.
+			paramList.add(paramList.get(1));
+
+			// Choose some item randomly from the rest of items
+			Object[] writeIds = randomlyChooseInParams(paramList, 2, totalReadCount + 1, writeCount - 1);
+			for (Object id : writeIds)
+				paramList.add(id);
+
+			// set the update value
+			for (int i = 0; i < writeCount; i++)
+				paramList.add(rvg.nextDouble() * 100000);
+		}
+
+		return paramList.toArray(new Object[0]);
+	}
+
+	/**
+	 * @param params
+	 * @param startIdx
+	 *            the starting index (inclusive)
+	 * @param endIdx
+	 *            the ending index (exclusive)
+	 * @param count
+	 * @return
+	 */
+	private Object[] randomlyChooseInParams(List<Object> params, int startIdx, int endIdx, int count) {
+		// Create a clone
+		Object[] tmps = new Object[endIdx - startIdx];
+		for (int i = 0; i < tmps.length; i++)
+			tmps[i] = params.get(startIdx + i);
+
+		// Swap
+		for (int times = 0; times < tmps.length * RANDOM_SWAP_FACTOR; times++) {
+			int pos = random.nextInt(tmps.length - 1);
+
+			Object tmp = tmps[pos];
+			tmps[pos] = tmps[pos + 1];
+			tmps[pos + 1] = tmp;
+		}
+
+		// Retrieve the first {count} results
+		Object[] results = new Integer[count];
+		for (int i = 0; i < count; i++)
+			results[i] = tmps[i];
+
+		return results;
+	}
+
+	private int randomChooseOtherPartition(int mainPartition, TpccValueGenerator rvg) {
+		return ((mainPartition + rvg.number(1, NUM_PARTITIONS - 1)) % NUM_PARTITIONS);
+	}
+
+	private void chooseHotData(List<Object> paramList, int partition, int count) {
+		int minMainPart = partition * DATA_SIZE_PER_PART;
+		RandomNonRepeatGenerator rg = new RandomNonRepeatGenerator(HOT_DATA_SIZE_PER_PART);
+		for (int i = 0; i < count; i++) {
+			int tmp = rg.next(); // 1 ~ size
+			int itemId = minMainPart + tmp;
+			paramList.add(itemId);
+		}
+
+	}
+
+	private void chooseCoaccessData(List<Object> paramList, int partition, int count) {
+		int minMainPart = partition * DATA_SIZE_PER_PART;
+		RandomNonRepeatGenerator rg = new RandomNonRepeatGenerator(COACCESS_RANGE);
+		for (int i = 0; i < count; i++) {
+			int tmp = rg.next(); // 1 ~ size
+			int itemId = minMainPart + tmp;
+			paramList.add(itemId);
+		}
+
+	}
+
+	private void chooseColdData(List<Object> paramList, int partition, int count) {
+		int minMainPartColdData = partition * DATA_SIZE_PER_PART + HOT_DATA_SIZE_PER_PART;
+		RandomNonRepeatGenerator rg = new RandomNonRepeatGenerator(COLD_DATA_SIZE_PER_PART);
+		for (int i = 0; i < count; i++) {
+			int tmp = rg.next(); // 1 ~ size
+			int itemId = minMainPartColdData + tmp;
+			paramList.add(itemId);
+		}
+
+	}
+}
