4cdd70f7ffe0ca91faadb44442b50c8f9dc35cb5
SLMT <sam123456777@gmail.com>
2017-07-04 17:37:17 +0800

    Fix the bugs in TPC-E

--- src/main/java/org/elasql/bench/server/procedure/calvin/tpce/TradeResultProc.java
+++ src/main/java/org/elasql/bench/server/procedure/calvin/tpce/TradeResultProc.java
@@ -14,7 +14,7 @@ import org.vanilladb.core.sql.VarcharConstant;
 
 public class TradeResultProc extends CalvinStoredProcedure<TradeResultParamHelper> {
 	
-	private RecordKey accountKey, customerKey, brokerKey, tradeKey, tradeHistoryKey;
+	private RecordKey cusAcctKey, customerKey, brokerKey, tradeKey, tradeHistoryKey;
 	
 	public TradeResultProc(long txNum) {
 		super(txNum, new TradeResultParamHelper());
@@ -25,9 +25,9 @@ public class TradeResultProc extends CalvinStoredProcedure<TradeResultParamHelpe
 		// account
 		Map<String, Constant> keyEntryMap = new HashMap<String, Constant>();
 		keyEntryMap.put("ca_id", new BigIntConstant(paramHelper.getAcctId()));
-		accountKey = new RecordKey("customer_account", keyEntryMap);
-		addReadKey(accountKey);
-		addWriteKey(accountKey);
+		cusAcctKey = new RecordKey("customer_account", keyEntryMap);
+		addReadKey(cusAcctKey);
+		addWriteKey(cusAcctKey);
 
 		// customer
 		keyEntryMap = new HashMap<String, Constant>();
@@ -53,43 +53,44 @@ public class TradeResultProc extends CalvinStoredProcedure<TradeResultParamHelpe
 		keyEntryMap
 				.put("th_t_id", new BigIntConstant(paramHelper.getTradeId()));
 		tradeHistoryKey = new RecordKey("trade_history", keyEntryMap);
-		addWriteKey(tradeHistoryKey);
+		addInsertKey(tradeHistoryKey);
 	}
 
 	@Override
 	protected void executeSql(Map<RecordKey, CachedRecord> readings) {
 		// SELECT ca_name, ca_b_id, ca_c_id FROM customer_account WHERE
 		// ca_id = acctId
-		String accountName = (String) readings.get(accountKey)
-				.getVal("ca_name").asJavaVal();
+		CachedRecord rec = readings.get(cusAcctKey);
+		rec.getVal("ca_name").asJavaVal();
+		rec.getVal("ca_b_id").asJavaVal();
+		rec.getVal("ca_c_id").asJavaVal();
 
 		// SELECT c_name FROM customer WHERE c_id = customerKey
-		String customerName = (String) readings.get(customerKey)
-				.getVal("c_name").asJavaVal();
+		rec = readings.get(customerKey);
+		rec.getVal("c_f_name").asJavaVal();
 
 		// SELECT b_name FROM broker WHERE b_id = brokerId
-		String brokerName = (String) readings.get(brokerKey)
-				.getVal("b_name").asJavaVal();
+		rec = readings.get(brokerKey);
+		rec.getVal("b_name").asJavaVal();
 
 		// SELECT trade_infos FROM trade WHERE t_id = tradeId
-		CachedRecord rec = readings.get(tradeKey);
-		long tradeTime = (Long) rec.getVal("t_dts").asJavaVal();
-		String symbol = (String) rec.getVal("t_s_symb").asJavaVal();
+		rec = readings.get(tradeKey);
+		rec.getVal("t_trade_price").asJavaVal();
 
-		// INSERT INTO trade_history(th_t_id, th_dts)
+		// INSERT INTO trade_history (th_t_id, th_dts, th_st_id) VALUES (...)
 		long currentTime = System.currentTimeMillis();
 		Map<String, Constant> fldVals = new HashMap<String, Constant>();
 		fldVals = new HashMap<String, Constant>();
 		fldVals.put("th_t_id", new BigIntConstant(paramHelper.getTradeId()));
 		fldVals.put("th_dts", new BigIntConstant(currentTime));
+		fldVals.put("th_st_id", new VarcharConstant("A"));
 		insert(tradeHistoryKey, fldVals);
 
 		// UPDATE customer_account SET ca_bal = ca_bal + tradePrice WHERE
 		// ca_id = acctId
-		rec = readings.get(accountKey);
-		rec.setVal("ca_bal", new DoubleConstant(1000));
-		rec.setVal("ca_name", new VarcharConstant(accountName));
-		update(accountKey, rec);
+		rec = readings.get(cusAcctKey);
+		rec.setVal("ca_bal", new DoubleConstant(1000.0));
+		update(cusAcctKey, rec);
 	}
 
 }
