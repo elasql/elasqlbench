c33a122604b39d4cc51dc3099117226e32ec6ae4
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2017-10-18 12:58:39 +0800

    change to real, set one round only

--- src/main/java/org/elasql/bench/rte/micro/ElasqlMicrobenchRte.java
+++ src/main/java/org/elasql/bench/rte/micro/ElasqlMicrobenchRte.java
@@ -14,7 +14,7 @@ public class ElasqlMicrobenchRte extends RemoteTerminalEmulator {
 
 	public ElasqlMicrobenchRte(SutConnection conn, StatisticMgr statMgr) {
 		super(conn, statMgr);
-		executor = new MicrobenchmarkTxExecutor(new ElasqlMiroChangingbenchmarkParamGen());
+		executor = new MicrobenchmarkTxExecutor(new ElasqlMicroRealisticbenchmarkParamGen());
 	}
 	
 	protected TransactionType getNextTxType() {
