b4219f5aa9f6cda29d4986804432f6d5cbc3a39d
SLMT <sam123456777@gmail.com>
2018-10-15 21:12:41 +0800

    Update for Google workloads

--- src/main/java/org/elasql/bench/rte/ycsb/ElasqlYcsbRte.java
+++ src/main/java/org/elasql/bench/rte/ycsb/ElasqlYcsbRte.java
@@ -15,8 +15,8 @@ public class ElasqlYcsbRte extends RemoteTerminalEmulator {
 	
 	public ElasqlYcsbRte(SutConnection conn, StatisticMgr statMgr, int nodeId) {
 		super(conn, statMgr);
-		executor = new YcsbTxExecutor(new MultiTanentsParamGen(nodeId));
-//		executor = new YcsbTxExecutor(new GoogleWorkloadsParamGen(nodeId));
+//		executor = new YcsbTxExecutor(new MultiTanentsParamGen(nodeId));
+		executor = new YcsbTxExecutor(new GoogleWorkloadsParamGen(nodeId));
 	}
 	
 	protected TransactionType getNextTxType() {
