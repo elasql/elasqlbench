7b7a583d23a5e3f3f20cc61f994a4df8eb31256f
SLMT <sam123456777@gmail.com>
2018-11-04 22:43:27 +0800

    Fix a bug

--- src/main/java/org/elasql/bench/server/metadata/migration/TpccAfterPartPlan.java
+++ src/main/java/org/elasql/bench/server/metadata/migration/TpccAfterPartPlan.java
@@ -2,7 +2,9 @@ package org.elasql.bench.server.metadata.migration;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.elasql.migration.MigrationRange;
 import org.elasql.storage.metadata.PartitionMetaMgr;
@@ -36,5 +38,30 @@ public class TpccAfterPartPlan extends TpccBeforePartPlan implements Serializabl
 		}
 		
 		return list;
+	}
+	
+	@Override
+	public String toString() {
+		Map<Integer, List<Integer>> wids = new HashMap<Integer, List<Integer>>();
+		for (int partId = 0; partId < PartitionMetaMgr.NUM_PARTITIONS; partId++)
+			wids.put(partId, new ArrayList<Integer>());
+		
+		for (int wid = 1; wid <= numOfWarehouses(); wid++)
+			wids.get(getPartition(wid)).add(wid);
+		
+		StringBuilder sb = new StringBuilder("TPC-C Plan: { Warehouse Ids:");
+		for (int partId = 0; partId < PartitionMetaMgr.NUM_PARTITIONS; partId++) {
+			sb.append(String.format("Part %d:[", partId));
+			
+			for (Integer wid : wids.get(partId))
+				sb.append(String.format("%d, ", wid));
+			sb.delete(sb.length() - 2, sb.length());
+			
+			sb.append("], ");
+		}
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append("}");
+		
+		return sb.toString();
 	}
 }
--- src/main/java/org/elasql/bench/server/metadata/migration/TpccBeforePartPlan.java
+++ src/main/java/org/elasql/bench/server/metadata/migration/TpccBeforePartPlan.java
@@ -1,15 +1,23 @@
 package org.elasql.bench.server.metadata.migration;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.elasql.bench.server.metadata.TpccPartitionPlan;
 import org.elasql.storage.metadata.PartitionMetaMgr;
 
 public class TpccBeforePartPlan extends TpccPartitionPlan {
 	
-	public static final int NORMAL_WAREHOUSE_PER_PART = 10;
+//	public static final int NORMAL_WAREHOUSE_PER_PART = 10;
+	public static final int NORMAL_WAREHOUSE_PER_PART = 3;
 	
 	// "HOT_WAREHOUSE_PER_HOT_PART * NUM_HOT_PARTS" should be <= NUM_PARTITIONS
-	public static final int HOT_WAREHOUSE_PER_HOT_PART = 3;
-	public static final int NUM_HOT_PARTS = 3;
+//	public static final int HOT_WAREHOUSE_PER_HOT_PART = 3;
+	public static final int HOT_WAREHOUSE_PER_HOT_PART = 2;
+//	public static final int NUM_HOT_PARTS = 3;
+	public static final int NUM_HOT_PARTS = 2;
 	
 	public static final int MAX_NORMAL_WID = NORMAL_WAREHOUSE_PER_PART
 			* PartitionMetaMgr.NUM_PARTITIONS;
@@ -30,6 +38,31 @@ public class TpccBeforePartPlan extends TpccPartitionPlan {
 		if (wid <= MAX_NORMAL_WID)
 			return (wid - 1) / NORMAL_WAREHOUSE_PER_PART;
 		else
-			return (wid - MAX_NORMAL_WID - 1) % HOT_WAREHOUSE_PER_HOT_PART;
+			return (wid - MAX_NORMAL_WID - 1) % NUM_HOT_PARTS;
+	}
+	
+	@Override
+	public String toString() {
+		Map<Integer, List<Integer>> wids = new HashMap<Integer, List<Integer>>();
+		for (int partId = 0; partId < PartitionMetaMgr.NUM_PARTITIONS; partId++)
+			wids.put(partId, new ArrayList<Integer>());
+		
+		for (int wid = 1; wid <= numOfWarehouses(); wid++)
+			wids.get(getPartition(wid)).add(wid);
+
+		StringBuilder sb = new StringBuilder("TPC-C Plan: { Warehouse Ids:");
+		for (int partId = 0; partId < PartitionMetaMgr.NUM_PARTITIONS; partId++) {
+			sb.append(String.format("Part %d:[", partId));
+			
+			for (Integer wid : wids.get(partId))
+				sb.append(String.format("%d, ", wid));
+			sb.delete(sb.length() - 2, sb.length());
+			
+			sb.append("], ");
+		}
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append("}");
+		
+		return sb.toString();
 	}
 }
